0dd76971c282fb82967eaeeda006eaf3cf9c99f7
Yassine Hermellin <yassine.hermellin@gmail.com>
2019-01-28 12:57:04 +0100

    About section (WIP)

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,6 +4,28 @@ import Section from '../Section'
 import FontSizes from '../../constants/FontSizes'
 import { SectionTitle } from '../Title'
 
+const Top = styled.div`
+  border: 1px solid red;
+  display: flex;
+  justify-content: flex-end;
+  position: relative;
+`;
+
+const Image = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 250px;
+  height: 250px;
+  background-color: darkolivegreen;
+`;
+
+const Content = styled.div`
+  background-color: lightblue;
+  margin-top: 50px;
+  min-height: 300px;
+`;
+
 export default class extends React.PureComponent {
   constructor (props) {
     super(props);
@@ -12,7 +34,13 @@ export default class extends React.PureComponent {
   render () {
     return (
       <Section>
-        <SectionTitle>Section</SectionTitle>
+          <Image />
+          <Top>
+            <SectionTitle>Section</SectionTitle>
+          </Top>
+        <Content>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam bibendum, dolor eget consectetur dictum, neque urna scelerisque diam, hendrerit gravida nisi lacus at justo. Duis porttitor turpis faucibus odio convallis semper. Praesent euismod nunc egestas augue dictum vestibulum. Morbi blandit sapien quis elit ultrices ultrices.
+        </Content>
       </Section>
     );
   }
--- src/components/Section.js
+++ src/components/Section.js
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   background-color: f5f5f5;
-  padding: 40px;
+  max-width: 1200px;
+  margin: auto;
+  position: relative;
 `;
 
 export default class extends React.PureComponent {
